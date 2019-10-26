6ad5df442a7c6d866b061cdc58b7ed9b105e3704
DonWaMams <pasekamonyeki@gmail.com>
2019-10-26 16:11:08 +0200

    centered form and table and updated table

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './App.css';
-import Table from 'react-bootstrap/Table';
-import Form from 'react-bootstrap/Form'
-
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Table1 from './table.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       repourl : '',
-      timespan : ''
+      timespan : '',
+      name: '',
+      canShow:false,
   };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,77 +28,54 @@ class NameForm extends React.Component {
   }
 
   handleSubmit(event) {
-    alert(this.state.repourl + " " +this.state.timespan);
+
+    this.setState({canShow: true});
+
     event.preventDefault();
   }
 
-
   render() {
+
+    let styles = {
+      
+       margin: 'auto',
+       width: '50%',
+       padding: '10px',
+    };
+
+    
     return (
-<div>
-<div>
-<form onSubmit={this.handleSubmit}>
 
-<Form.Group controlId="formHorizontalEmail">
-    <Form.Label>
-      Email
-    </Form.Label>
-    <Col sm={10}>
-      <Form.Control type="email" placeholder="Email" />
-    </Col>
+<div style={styles}>
+<div>
+<Form  onSubmit={this.handleSubmit}>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Repo Url :</Form.Label>
+    <Form.Control value={this.state.repourl} onChange={this.handleRepoChange}/>
   </Form.Group>
 
-  <Form.Group as={Row} controlId="formHorizontalPassword">
-    <Form.Label column sm={2}>   
-      Password
-    </Form.Label>
-    <Col sm={10}>
-      <Form.Control type="password" placeholder="Password" />
-    </Col>
-  </Form.Group>
-
-        <label>
-          Repo Url :
-          <input type="text" value={this.state.repourl} onChange={this.handleRepoChange} />
-        </label>
-          <br />
-        <label>
-          Timespan :
-        </label>
-              <select onChange={this.handleTimespanChange}>
-                <option value="24Hours">24 hours</option>
-                <option value="lastweek">last week</option>
-                <option value="lastmonth">last month</option>
-              </select>
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Timespan :</Form.Label>
+    <Form.Control as="select" onChange={this.handleTimespanChange}>
+                <option value="24hours">24 hours</option>
+                <option value="week">last week</option>
+                <option value="month">last month</option>
+                </Form.Control>
  
-        <input type="submit" value="Submit" />
-  <Button variant="primary" type="submit">
+  </Form.Group>
+  <Form.Group>
+  <Button type="submit" value="Submit">
     Submit
   </Button>
-      </form>
+  </Form.Group>
+  </Form>
+</div>
+<div>
+      <Table1 canShow={this.state.canShow} interval={this.state.timespan}></Table1>
+</div>
 </div>
 
-<div>
-<Table>
-<thead>
-  <tr>
-    <th>Username</th>
-    <th>Commmit count</th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <td>Mark</td>
-    <td>Otto</td>
-  </tr>
-  <tr>
-    <td>Jacob</td>
-    <td>Thornton</td>
-  </tr>
-</tbody>
-</Table>
-</div>
-</div>
+
     );
   }
 }
--- src/table.js
+++ src/table.js
@@ -1,103 +1,135 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Table from 'react-bootstrap/Table';
+import axios from 'axios';
 
-class Table extends Component {
+
+
+class Table1 extends Component {
    constructor(props) {
       super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
       this.state = { //state is by default an object
-         students: [
-            { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-            { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-            { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-            { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
-         ]
+         interval :'',
+         data : null,
       }
    }
- 
-   renderTableData() {
-    return this.state.students.map((student, index) => {
-       const { id, name, age, email } = student //destructuring
-       return (
-          <tr key={id}>
-             <td>{id}</td>
-             <td>{name}</td>
-             <td>{age}</td>
-             <td>{email}</td>
-          </tr>
-       )
+
+
+ componentDidMount() {
+   
+      let interval = this.props.interval;
+   axios.get('http://localhost:8080/',
+    {params: {interval: this.props.interval}},
+    {headers: { "Content-Type": "application/json;charset=UTF-8"}
     })
- }
- 
- renderTableHeader() {
-    let header = Object.keys(this.state.students[0])
-    return header.map((key, index) => {
-       return <th key={index}>{key.toUpperCase()}</th>
+    .then(function (response) {
+
+        console.log(response.data.CommitCounts);
     })
- }
+    .catch(function (error) {
+      console.log(error);
+    });
+
+   }
  
  render() {
+
+   this.componentDidMount();
+   let resultTable;
+
+   if(this.props.canShow){
+     if(this.props.interval === "24hours"){
+      resultTable =
+      <Table>
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>Commmit count</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>paseka</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>trueadm</td>
+          <td>3</td>
+        </tr>
+      </tbody>
+      </Table>;
+     }
+
+     else if(this.props.interval === "week"){
+      resultTable =
+      <Table>
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>Commmit count</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>acdlite </td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>paseka</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>trueadm</td>
+          <td>3</td>
+        </tr>
+      </tbody>
+      </Table>;
+     }
+
+     else if(this.props.interval === "month"){
+
+      resultTable =
+      <Table>
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>Commmit count</th>
+        </tr>
+      </thead>
+      <tbody>
+      <tr>
+          <td>acdlite </td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>paseka</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>trueadm</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>bvaughn</td>
+          <td>2</td>
+        </tr>
+      </tbody>
+      </Table>;
+
+     }
+   }
+   else{
+
+      resultTable = "";
+   }
+
+   
     return (
        <div>
-          <h1 id='title'>React Dynamic Table</h1>
-          <table id='students'>
-             <tbody>
-                {this.renderTableData()}
-             </tbody>
-          </table>
-       </div>
+         {resultTable}
+         </div>
     )
- }
- }
+ }   
  
- class NameForm extends React.Component {
-   constructor(props) {
-     super(props);
-     this.state = {
-       repourl : '',
-       timespan : ''
-   };
- 
-     this.handleSubmit = this.handleSubmit.bind(this);
-   }
- 
-   handleRepoChange = (event) => { 
-     this.setState({repourl: event.target.value});
-   }
- 
-   handleTimespanChange =(event) =>{
-     this.setState({timespan: event.target.value});
-   }
- 
-   handleSubmit(event) {
-     alert(this.state.repourl + " " +this.state.timespan);
-     event.preventDefault();
-   }
- 
- 
-   render() {
-     return (
-       <div>
-       <Table />
-       </div>
-
-       <form onSubmit={this.handleSubmit}>
-         <label>
-           Repo Url :
-           <input type="text" value={this.state.repourl} onChange={this.handleRepoChange} />
-         </label>
- 
-         <label>
-           Timespan :
-         </label>
-               <select onChange={this.handleTimespanChange}>
-                 <option value="24Hours">24 hours</option>
-                 <option value="lastweek">last week</option>
-                 <option value="lastmonth">last month</option>
-               </select>
-  
-         <input type="submit" value="Submit" />
-       </form>
-     );
-   }
  }
 
-export default Table;
+export default Table1;
