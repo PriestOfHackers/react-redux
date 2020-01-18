f294e96a62833a1550d74cc603ac73d706544034
DonWaMams <pasekamonyeki@gmail.com>
2020-01-18 16:54:18 +0200

    added new add photo button

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,11 +1,17 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Photo from './Photo'
 
 
 function PhotoWall(props){
-    return <div className="photoGrid">
-    {props.posts.map((post , index) => <Photo key={index} post={post}/>)}
+
+ return<div>
+        <a onClick={props.onNavigate} className="addIcon" href="#addPhoto"></a> 
+        <div className="photoGrid">
+        {props.posts.map((post , index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+        </div>
     </div>     
 }
+
+
 
 export default PhotoWall;
