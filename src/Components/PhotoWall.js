dc2c65f91c4a7dbc9ba03799aed7422051cc4897
DonWaMams <pasekamonyeki@gmail.com>
2020-02-18 17:29:03 +0200

    added package for styled components

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import Photo from './Photo'
 import {Link} from 'react-router-dom'
+//import styled from 'styled-components';
 
 
+//how to use inline styles for particular elements
+/*const photo =  styled.div` 
+    width: calc(100% + 4rem);
+    margin-left: -2rem;
+    margin-top: -2rem;
+    max-width: none;
+   `;*/
+   
 function PhotoWall(props){
 
  return<div>
